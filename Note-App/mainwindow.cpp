298b98345ff0740daeaa31b6962e22900a320bcd
Vladyslav <itochanenko@gmail.com>
2018-11-03 16:59:20 +0200

    Added interface. Tag system is working

--- Note-App/mainwindow.cpp
+++ Note-App/mainwindow.cpp
@@ -1,12 +1,83 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+#include <QInputDialog>
+#include <QMessageBox>
+
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow) {
     ui->setupUi(this);
+
+    // Right button for list
+    ui->list_tags->setContextMenuPolicy(Qt::CustomContextMenu);
+        connect(ui->list_tags, SIGNAL(customContextMenuRequested(QPoint)), this, SLOT(showContextMenu(QPoint)));
 }
 
 MainWindow::~MainWindow() {
     delete ui;
+}
+
+// Funciton to add new item
+void MainWindow::addTagFunction() {
+    bool ok;
+    QString tagText = QInputDialog::getText(this, tr("Create New Tag"), tr("Tag name:"), QLineEdit::Normal, "default", &ok);
+    tagText = tagText.toLower();
+    if (ok && !tagText.isEmpty()) {
+        // Chek for similar tag
+        int numberOfTags = ui->list_tags->count();
+        bool hasSame = false;
+        for (int i = 0; i < numberOfTags; i++)
+            if (ui->list_tags->item(i)->text() == tagText)
+                hasSame = true;
+        if (!hasSame)
+            ui->list_tags->addItem(tagText);
+        else {
+            // If tag is already available
+            QMessageBox messageBox;
+            messageBox.critical(nullptr,"Error","There is already a \"" + tagText + "\" tag!");
+            messageBox.setFixedSize(600,200);
+        }
+    }
+}
+
+void MainWindow::on_button_newTag_clicked() {
+    addTagFunction();
+}
+
+void MainWindow::on_button_deleteTag_clicked() {
+
+}
+
+void MainWindow::on_button_backup_clicked() {
+
+}
+
+void MainWindow::on_button_newNote_clicked() {
+
+}
+
+void MainWindow::on_button_openArchive_clicked() {
+
+}
+
+void MainWindow::showContextMenu(const QPoint &pos) {
+    QPoint globalPos = ui->list_tags->mapToGlobal(pos);
+
+    QMenu myMenu;
+    myMenu.addAction("Insert", this, SLOT(addItem()));
+    myMenu.addAction("Delete",  this, SLOT(eraseItem()));
+
+    myMenu.exec(globalPos);
+}
+
+void MainWindow::addItem() {
+    addTagFunction();
+}
+
+void MainWindow::eraseItem() {
+    for (int i = 0; i < ui->list_tags->selectedItems().size(); ++i) {
+        QListWidgetItem *item = ui->list_tags->takeItem(ui->list_tags->currentRow());
+        delete item;
+    }
 }
--- Note-App/mainwindow.h
+++ Note-App/mainwindow.h
@@ -2,6 +2,7 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QString>
 
 namespace Ui {
     class MainWindow;
@@ -13,6 +14,19 @@ class MainWindow : public QMainWindow {
 public:
     explicit MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
+
+    void addTagFunction();
+private slots:
+    void on_button_newTag_clicked();
+    void on_button_deleteTag_clicked();
+    void on_button_newNote_clicked();
+    void on_button_openArchive_clicked();
+    void on_button_backup_clicked();
+
+    void showContextMenu(const QPoint&);
+
+    void eraseItem();
+    void addItem();
 
 private:
     Ui::MainWindow *ui;
