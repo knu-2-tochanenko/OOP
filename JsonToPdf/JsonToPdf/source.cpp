eb923c15dbe18eb49c6f737510122e1728a5da6b
Vladyslav <itochanenko@gmail.com>
2018-12-01 16:07:05 +0200

    Added RJGP class

--- JsonToPdf/JsonToPdf/source.cpp
+++ JsonToPdf/JsonToPdf/source.cpp
@@ -11,22 +11,36 @@ using namespace std;
 
 using json = nlohmann::json;
 
+class RJDP { // Read JSON, generate PDF
+private:
+	vector<json> notes;
+	vector<json> archive;
+	vector<string> tags;
+public:
+	bool read(string const filename) {
+		std::ifstream jsonInput(filename);
+		json jsonObject;
+		jsonInput >> jsonObject;
+
+		json notesJson = jsonObject["notes"];
+		json archiveJson = jsonObject["archive"];
+		json tagsJson = jsonObject["tags"];
+
+		this->notes = notesJson.get<vector<json>>();
+		this->archive = archiveJson.get<vector<json>>();
+		this->tags = tagsJson.get<vector<string>>();
+		return true;
+	}
+
+	bool generate() const {
+		return true;
+	}
+};
+
 int main() {
-
-	// read a JSON file
-	std::ifstream jsonInput("backup.json");
-	json jsonObject;
-	jsonInput >> jsonObject;
-
-	json notes = jsonObject["notes"];
-	json archive = jsonObject["archive"];
-	json maxID = jsonObject["max_id"];
-	json tags = jsonObject["tags"];
-
-	cout << notes << endl;
-	cout << archive << endl;
-	cout << maxID << endl;
-	cout << tags << endl;
+	RJDP rjdp;
+	rjdp.read("backup.json");
+	
 
 	system("pause");
 	return 0;
