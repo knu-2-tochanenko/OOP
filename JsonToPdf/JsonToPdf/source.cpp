dd9c0791f991d280c0ad650ca2d3ce681468cd39
Vladyslav <itochanenko@gmail.com>
2018-12-01 21:26:47 +0200

    Added generation of PDF file & 4 new features
    Added to_string function to convert string -> char* const
    Added writeNote method, which writes single note
    Added printNotesString method to write text beautifully
    Added support for FUP arguments system

--- JsonToPdf/JsonToPdf/source.cpp
+++ JsonToPdf/JsonToPdf/source.cpp
@@ -1,15 +1,43 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
 #include <setjmp.h>
 #include <iostream>
 #include <fstream>
 #include <iomanip> 
 #include <vector>
 #include <string>
+#include <cstring>
 #include "hpdf.h"
 #include "json.hpp"
 
 using namespace std;
 
 using json = nlohmann::json;
+//////////////////////////////////////////////////////
+jmp_buf env;
+
+#ifdef HPDF_DLL
+void  __stdcall
+#else
+void
+#endif
+error_handler(HPDF_STATUS   error_no,
+	HPDF_STATUS   detail_no,
+	void         *user_data) {
+	printf("ERROR: error_no=%04X, detail_no=%u\n", (HPDF_UINT)error_no,
+		(HPDF_UINT)detail_no);
+	longjmp(env, 1);
+}
+
+const char *font_list[] = {
+	"Courier",
+	"Courier-Oblique",
+	NULL
+};
+//////////////////////////////////////////////////////
 
 struct SingleNote {
 	string text;
@@ -28,7 +56,6 @@ struct SingleNote {
 		this->editedDate = editedDate;
 		this->tags = tags;
 	}
-
 };
 
 class RJDP { // Read JSON, generate PDF
@@ -63,6 +90,42 @@ private:
 		}
 	}
 
+	char* const to_string(string const str) const {
+		char *res;
+		res = new char[str.size()];
+		strcpy(res, str.c_str());
+		cout << res << endl;
+		return res;
+	}
+
+	void printNotesText(string const & text, HPDF_Page page) const {
+		// Max length - 57
+		int strLen = text.size(), charStringSize = 0;
+		string charString;
+		for (int i = 0; i < strLen; i++) {
+			if ((text[i] == '\n') || (charString.size() > 56)) {
+				HPDF_Page_ShowText(page, to_string(charString));
+				HPDF_Page_MoveTextPos(page, 0, -20);
+				charString = "";
+			}
+			else
+				charString += text[i];
+		}
+		if (charString.size() > 0) {
+			HPDF_Page_ShowText(page, to_string(charString));
+			HPDF_Page_MoveTextPos(page, 0, -20);
+		}
+	}
+
+	// add &
+	void writeNote(SingleNote* const sn, int shift, HPDF_Doc pdf, HPDF_Page page, HPDF_Font def_font) const {
+		HPDF_Font font = HPDF_GetFont(pdf, font_list[0], NULL);
+
+		/* print a sample text. */
+		HPDF_Page_SetFontAndSize(page, font, 14);
+		printNotesText(sn->text, page);
+	}
+
 public:
 	bool read(string const filename) {
 		std::ifstream jsonInput(filename);
@@ -79,21 +142,93 @@ public:
 		jsonToVecNotes(this->archive, archiveJson);
 		this->tags = tagsJson.get<vector<string>>();
 
-		testVecNotes(this->notes, "NOTES");
-		testVecNotes(this->archive, "ARCHIVE");
+//		testVecNotes(this->notes, "NOTES");
+//		testVecNotes(this->archive, "ARCHIVE");
 
 		return true;
 	}
 
-	bool generate() const {
+	bool generate(string filepath = "D:/", string username = "", string password = "") {
+		cout << filepath << endl;
+		cout << username << endl;
+		cout << password << endl;
+
+		const char *notesText = "Notes";
+		const char *archiveText = "Archived Notes";
+		HPDF_Doc  pdf;
+		string filename = "D:\\Vladislav\\Documents\\file.pdf";
+		char fname[256];
+		strcpy(fname, filename.c_str());
+		HPDF_Page page;
+		HPDF_Font def_font;
+		HPDF_REAL tw;
+		HPDF_REAL height;
+		HPDF_REAL width;
+		HPDF_UINT i;
+
+		pdf = HPDF_New(error_handler, NULL);
+		if (!pdf) {
+			printf("error: cannot create PdfDoc object\n");
+			return 1;
+		}
+
+		if (setjmp(env)) {
+			HPDF_Free(pdf);
+			return 1;
+		}
+
+		/* Add a new page object. */
+		page = HPDF_AddPage(pdf);
+
+		height = HPDF_Page_GetHeight(page);
+		width = HPDF_Page_GetWidth(page);
+
+		/* Print the title of the page (with positioning center). */
+		def_font = HPDF_GetFont(pdf, "Helvetica", NULL);
+		HPDF_Page_SetFontAndSize(page, def_font, 24);
+
+		tw = HPDF_Page_TextWidth(page, notesText);
+		HPDF_Page_BeginText(page);
+		HPDF_Page_TextOut(page, (width - tw) / 2, height - 50, notesText);
+		HPDF_Page_EndText(page);
+
+		// Write notes
+		HPDF_Page_BeginText(page);
+		HPDF_Page_MoveTextPos(page, 60, height - 105);
+		for (int i = 0; i < this->notes.size(); i++)
+			writeNote(this->notes[i], -20, pdf, page, def_font);
+		HPDF_Page_EndText(page);
+
+		/* Print the title of the page (with positioning center). */
+		def_font = HPDF_GetFont(pdf, "Helvetica", NULL);
+		HPDF_Page_SetFontAndSize(page, def_font, 24);
+
+		tw = HPDF_Page_TextWidth(page, archiveText);
+		HPDF_Page_BeginText(page);
+		HPDF_Page_TextOut(page, (width - tw) / 2, height - 100, archiveText);
+		HPDF_Page_EndText(page);
+		
+
+		HPDF_SaveToFile(pdf, fname);
+
+		/* clean up */
+		HPDF_Free(pdf);
+
 		return true;
 	}
 };
 
-int main() {
+//	Uses FUP (filepath username password)
+int main(int argumentsCount, char** arguments) {
 	RJDP rjdp;
 	rjdp.read("backup.json");
 	
+	if (argumentsCount == 2)
+		rjdp.generate(arguments[1]);
+	else if (argumentsCount == 4) {
+		rjdp.generate(arguments[1], arguments[2], arguments[3]);
+	}
+	else return 1;
 
 	system("pause");
 	return 0;
