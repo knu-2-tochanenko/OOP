fc9ff6eedb0848f22127fddb0bf51f50e032c2de
Vladislav <itochanenko@gmail.com>
2019-05-27 01:54:57 +0300

    Tested HexConverter and StringParser

--- UML/2_lab/tests.cpp
+++ UML/2_lab/tests.cpp
@@ -6,8 +6,73 @@
 #include "catch.hpp"
 #include "graphlist.h"
 #include "graphmatrix.h"
+#include "hexconverter.h"
+#include "stringparser.h"
+#include "ipconverter.h"
+#include "ip.h"
 
-TEST_CASE( "Distance between nodes", "[distance]" ) {
+#include <vector>
+#include <string>
+
+using std::vector;
+using std::string;
+
+TEST_CASE("Hex converter", "[hex][converter]") {
+    SECTION("Int to HEX") {
+        REQUIRE(HexConverter::toHex(15) == "F");
+        REQUIRE(HexConverter::toHex(0) == "0");
+        REQUIRE(HexConverter::toHex(8) == "8");
+        REQUIRE(HexConverter::toHex(2564) == "A04");
+    }
+    SECTION("HEX to Int") {
+        REQUIRE(HexConverter::toInt("F") == 15);
+        REQUIRE(HexConverter::toInt("0") == 0);
+        REQUIRE(HexConverter::toInt("8") == 8);
+        REQUIRE(HexConverter::toInt("A04") == 2564);
+    }
+}
+
+TEST_CASE ("String parser", "[parser]") {
+    vector<string> first;
+    SECTION("127.0.0.1") {
+        first.emplace_back("127");
+        first.emplace_back("0");
+        first.emplace_back("0");
+        first.emplace_back("1");
+        REQUIRE(StringParser::separate("127.0.0.1", '.') == first);
+    }
+    SECTION("255.255.255.255") {
+        first.emplace_back("255");
+        first.emplace_back("255");
+        first.emplace_back("255");
+        first.emplace_back("255");
+        REQUIRE(StringParser::separate("255.255.255.255", '.') == first);
+    }
+    SECTION("c86c:e301:7241:815:27c8:fae0:5049:10c9") {
+        first.emplace_back("c86c");
+        first.emplace_back("e301");
+        first.emplace_back("7241");
+        first.emplace_back("815");
+        first.emplace_back("27c8");
+        first.emplace_back("fae0");
+        first.emplace_back("5049");
+        first.emplace_back("10c9");
+        REQUIRE(StringParser::separate("c86c:e301:7241:815:27c8:fae0:5049:10c9", ':') == first);
+    }
+    SECTION("0:0:0:0:0:0:0:0") {
+        first.emplace_back("0");
+        first.emplace_back("0");
+        first.emplace_back("0");
+        first.emplace_back("0");
+        first.emplace_back("0");
+        first.emplace_back("0");
+        first.emplace_back("0");
+        first.emplace_back("0");
+        REQUIRE(StringParser::separate("0:0:0:0:0:0:0:0", ':') == first);
+    }
+}
+
+TEST_CASE("Distance between nodes", "[distance]") {
     GraphList<char> gl;
     gl.add('a');
     gl.add('b');
